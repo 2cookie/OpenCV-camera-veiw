23a796e95fa0366e94a026ef4b40184549fc170d
2cookie <qaandrain@outlook.com>
2024-11-20 20:56:49 -0700

    Add files via upload

---
+++ cam test - Copy.py
@@ -0,0 +1,22 @@
+import cv2
+
+# Initialize webcam
+id = input("Please enter the number ID of the webcam you want to tuse (if youre not sure input 0 to use the windows default webcam :p)")
+cap = cv2.VideoCapture(id)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Display the resulting frame
+    cv2.imshow('Webcam Live Feed', frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
